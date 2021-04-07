041be1d46c5c5a98f6031dbb88d2ec5b83bd7855
saswatsamal <samalsaswat0@gmail.com>
2021-04-07 12:49:46 +0530

    font and dimension of the name changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,22 +21,22 @@ const generatePDF = async (name) => {
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
     pdfDoc.registerFontkit(fontkit);
 
-    const fontBytes = await fetch("./assets/ttf/Sanchez-Regular.ttf").then((res) =>
+    const fontBytes = await fetch("./assets/ttf/Montserrat-ExtraLight.ttf").then((res) =>
     res.arrayBuffer()
   );
     // Embed our custom font in the document
-    const SanChezFont  = await pdfDoc.embedFont(fontBytes);
+    const Montserrat  = await pdfDoc.embedFont(fontBytes);
      // Get the first page of the document
      const pages = pdfDoc.getPages();
      const firstPage = pages[0];
    
      // Draw a string of text diagonally across the first page
      firstPage.drawText(name, {
-       x: 300,
-       y: 270,
-       size: 58,
-       font: SanChezFont ,
-       color: rgb(0.2, 0.84, 0.67),
+       x: 120,
+       y: 240,
+       size: 40,
+       font: Montserrat ,
+       color: rgb(0.0, 0.0, 0.0),
      });
    
     // Serialize the PDFDocument to bytes (a Uint8Array)
