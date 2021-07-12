e743cd732590c2595040ec367b334e8411093b8e
saswatsamal <samalsaswat0@gmail.com>
2021-07-13 00:15:07 +0530

    Codathon 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,24 +7,24 @@ const { PDFDocument, rgb, degrees } = PDFLib;
 submitBtn.addEventListener("click", () => {
     const val =userName.value;
     const val2 =userValidation.value;
-    if (val.trim() !== "" && userName.checkValidity() && val2=="AMA03") {
+    if (val.trim() !== "" && userName.checkValidity() && val2=="cdh2021wd") {
         generatePDF(val);
         text = "Downloading.";
       } 
-      if(val.trim() == "" && val2.trim() == "" && val2!=="AMA03"){
+      if(val.trim() == "" && val2.trim() == "" && val2!=="cdh2021wd"){
         text = "Enter your name and the correct Validation key";;
       }
       else if(val.trim() == ""){
         text = "Enter your name";;
       }
-      else if(val2.trim() == "" && val2!=="AMA03"){
+      else if(val2.trim() == "" && val2!=="cdh2021wd"){
         text = "Enter the correct Validation key";;
       }
       document.getElementById("alert").innerHTML = text;
 });
 
 const generatePDF = async (name) => {
-    const existingPdfBytes = await fetch("./assets/certificate/CER-HCI-AMA-2021-03-P.pdf").then((res) =>
+    const existingPdfBytes = await fetch("./assets/certificate/CER-HCI-CM-2021-01-P.pdf").then((res) =>
       res.arrayBuffer()
     );
 
@@ -43,7 +43,7 @@ const generatePDF = async (name) => {
    
      // Draw a string of text diagonally across the first page
      firstPage.drawText(name, {
-       x: 100,
+       x: 150,
        y: 315,
        size: 40,
        font: Montserrat ,
@@ -52,5 +52,5 @@ const generatePDF = async (name) => {
    
     // Serialize the PDFDocument to bytes (a Uint8Array)
     const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
-    saveAs(pdfDataUri,name + " CER-HCI-AMA-2021-03-P")
+    saveAs(pdfDataUri,name + " CER-HCI-CM-2021-01-P")
   };
